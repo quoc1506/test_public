dd833cc2fd03bb811b2f2daad3e618043c8c2f81
quoc1506 <nguyenvanquoc.02b@gmail.com>
2020-02-17 10:15:32 +0700

    Create user.js

---
+++ user.js
@@ -0,0 +1,48 @@
+function checkUser(type) {
+	let user;
+	switch (type) {
+		case 1: // daily
+			user = {username: ".sv@4525424;vn.", password: "Mt+/5ULOAw6tt5WxUi0pgm9+le0A1rL55GzUrvr/orE="}
+			break;
+		case 2: // weekly
+			user = {username: "purevpn0s8985611", password: "7qouhgmh"}
+			break;
+		case 3:
+			user = {username: "UVPNv2-b97d2b5b87537150e831f1de8cdace0dd1bad2a381557bd1c4dc300163cb3644&nguyenlinh6605+5@gmail.com", password: "a69b3e8091971fcf8ce988f42f4e7289405c2a83b0f2e262b1bf1f29a9e5df33"}
+			break;
+		case 4:
+			user = {username: "*cookie_ZHEuIFdfZCMjI2xQRnJpR2pmfFg5YyhySEhQIyMjbTpUPS9uS1dhVW1VMW4tWixPXT1WTUw6_chromeapp_3_Mon Dec 16 2019 11:07:38 GMT+0700 (Indochina Time)", password: ""}
+			break;
+		case 5:
+			user = {username: "Z42myeXkKRfDU2TGNJDxhcCu", password: "HNxyRvSNcsiC5jFGt3dmpVN4"}
+			break;
+		case 6: // limit storage
+			user = {username: "nguyenlinh6605@gmail.com", password: "e3658b80cf986109c366fea29c1a1858"}
+			break;
+		case 7:
+			user = {username: "DictOciafDocyangEzbawuj1", password: "bavpensacpyubEfketDiwrir"}
+			break;
+		case 8:
+			user = {username: "access_token", password: "dkrp6i7bssgv8p4gtdrnnjlne08rmoqukaghm7c930v7jkq7f5d"}
+			break;
+		case 9:
+			user  = {username: "bDZ0NW54ZjEtNHQ3dm5taA==", password: "NWM3MmI4ZHZrbg=="}
+			break;
+		case 10:
+			user = {username: "Z42myeXkKRfDU2TGNJDxhcCu", password: "HNxyRvSNcsiC5jFGt3dmpVN4"}
+			break;
+		case 11:
+			user = {username: "KS1-2f605abfea8a46850e68a22b17d0d3b9&nguyenlinh6605@gmail.com", password: "VYnYvrzOxboF"}
+			break;
+		case 12:
+			user = {username: "user-uuid-bb00e8025fbd7be1985da24fb860cf79", password: "0d411c082128"}
+			break;
+		case 13:
+			user = {username: "urbanvpn@urban-vpn.com", password: "urbanvpn4321"}
+			break;
+		default: 
+			user = {};
+			break;
+	}
+	return user;
+}
