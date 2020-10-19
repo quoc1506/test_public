da1ce5353091ca17a05c7d729a71191d0a33bf82
quoc1506 <nguyenvanquoc.02b@gmail.com>
2020-10-19 16:57:01 +0700

    Update user.js

--- user.js
+++ user.js
@@ -1,17 +1,17 @@
 function checkUser(type) {
 	let user;
 	switch (type) {
-		case 1: // daily // 18h
-			user = {username: ".sv@9066529;vn.", password: "hYSPQfdSIPZMBeWi42egur/V6aamNINalQgKJ4y/ixU="};
+		case 1: // daily // 17h
+			user = {username: ".sv@9066529;vn.", password: "hYSPQfdSIPb0T6sXrGHs7hGYdZDCJMryhVo32cKJtiM="};
 			break;
 		case 15: // dailyibvpn
-			user = {username: "hot09439@eoopy.com", password: "3aVCuvscGj"};
+			user = {username: "rtn40940@bcaoo.com", password: "yRH3jFFWo1"};
 			break;
 		case 16: // 3 day 17h 17/10
 			user = {username: "ffn72874@bcaoo.com", password: "ffn72874@bcaoo.com"};
 			break;
-		case 2: // weekly 12h 13/10
-			user = {username: "purevpn0s9854207", password: "72hyk25h"};
+		case 2: // weekly 17h 19/10
+			user = {username: "purevpn0s9870017", password: "bqcgahmd"};
 			break;
 		case 3:
 			user = {username: "UVPNv2-b97d2b5b87537150e831f1de8cdace0dd1bad2a381557bd1c4dc300163cb3644&nguyenlinh6605+5@gmail.com", password: "a69b3e8091971fcf8ce988f42f4e7289405c2a83b0f2e262b1bf1f29a9e5df33"};
