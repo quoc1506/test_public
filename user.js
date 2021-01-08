c009374f27a4e6d8da049ad259b4a3d8deb94c2c
quoc1506 <nguyenvanquoc.02b@gmail.com>
2021-01-08 16:05:15 +0700

    Update user.js

--- user.js
+++ user.js
@@ -7,11 +7,11 @@ function checkUser(type) {
 		case 15: // dailyibvpn
 			user = {username: "yng25533@eoopy.com", password: "TnnYR4xFwE"};
 			break;
-		case 16: // 3 day 17h 05/01
-			user = {username: "zot36090@zwoho.com", password: "zot36090@zwoho.com"};
+		case 16: // 3 day 16h 08/01
+			user = {username: "guo07381@zwoho.com", password: "guo07381@zwoho.com"};
 			break;
-		case 2: // weekly 9h 03/01
-			user = {username: "purevpn0s10093416", password: "2jpmwsps"};
+		case 2: // weekly 16h 08/01
+			user = {username: "purevpn0s10105694", password: "bioj2hqw"};
 			break;
 		case 3:
 			user = {username: "UVPNv2-b97d2b5b87537150e831f1de8cdace0dd1bad2a381557bd1c4dc300163cb3644&nguyenlinh6605+5@gmail.com", password: "a69b3e8091971fcf8ce988f42f4e7289405c2a83b0f2e262b1bf1f29a9e5df33"};
